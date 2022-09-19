66f188859fb0f120f35571bf85516d3bf590890a
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2022-09-19 23:56:30 +1000

    initial repo proxy

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { serve } from "https://deno.land/std@0.156.0/http/server.ts";
+
+const DEFAULT_BRANCH = "main";
+
+const pat = new URLPattern({
+  pathname: "/:proj([0-9A-Za-z_.\-]+(?:@[0-9A-Za-z_.\-]+)?)/:path*",
+});
+// "https://code.sunsetkookaburra.xyz/bitbin@0.0.3/formats/mod.ts"
+function remap(url: string) {
+  const result = pat.exec(url);
+  if (result === null) {
+    return new Response(null, { status: 404 });
+  } else {
+    const [project, branch] = result.pathname.groups.proj.split("@") as [
+      string,
+      string?,
+    ];
+    const path = result.pathname.groups.path;
+    return fetch(
+      `https://raw.githubusercontent.com/sunsetkookaburra/${project}/${
+        branch ?? DEFAULT_BRANCH
+      }/${path}`,
+    );
+  }
+}
+
+serve((req) => remap(req.url));
